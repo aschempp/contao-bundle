9393cc99a7d926023c58a4327793a647c1bb152a
Leo Feyer <github@contao.org>
2014-07-18 20:24:35 +0200

    Register the Contao configuration parameters in the Symfony container

--- src/DependencyInjection/ContaoExtension.php
+++ src/DependencyInjection/ContaoExtension.php
@@ -12,6 +12,7 @@
 
 namespace Contao\ContaoBundle\DependencyInjection;
 
+use Contao\Config;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -32,5 +33,38 @@ class ContaoExtension extends Extension
         );
 
         $loader->load('services.yml');
+
+        if (!defined('TL_ROOT')) {
+            define('TL_ROOT', dirname(dirname(dirname(dirname(dirname(__DIR__))))));
+        }
+
+        // Preload the Contao configuration if not done yet
+        if (!isset($GLOBALS['TL_CONFIG'])) {
+            Config::preload();
+        }
+
+        // Complete the database settings
+        $container->setParameter('database_driver', 'pdo_mysql');
+        $container->setParameter('database_host', $GLOBALS['TL_CONFIG']['dbHost']);
+        $container->setParameter('database_port', $GLOBALS['TL_CONFIG']['dbPort']);
+        $container->setParameter('database_name', $GLOBALS['TL_CONFIG']['dbDatabase']);
+        $container->setParameter('database_user', $GLOBALS['TL_CONFIG']['dbUser']);
+        $container->setParameter('database_password', $GLOBALS['TL_CONFIG']['dbPass']);
+
+        // Complete the mailer settings
+        if (!$GLOBALS['TL_CONFIG']['useSMTP']) {
+            $container->setParameter('mailer_transport', 'mail');
+        } else {
+            $container->setParameter('mailer_transport', 'smtp');
+            $container->setParameter('mailer_host', $GLOBALS['TL_CONFIG']['smtpHost']);
+            $container->setParameter('mailer_user', $GLOBALS['TL_CONFIG']['smtpUser']);
+            $container->setParameter('mailer_password', $GLOBALS['TL_CONFIG']['smtpPass']);
+        }
+
+        // Use the encryption key as kernel secret
+        $container->setParameter('kernel.secret', $GLOBALS['TL_CONFIG']['encryptionKey']);
+
+        // Set the default charset
+        $container->setParameter('kernel.charset', $GLOBALS['TL_CONFIG']['characterSet']);
     }
 }
