0c44e5e105e2cdaf2584820b77242ecad0c73b57
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-07-23 06:54:34 +0200

    Move the Contao initialization logic into the bundle class
    Updated copyright statement

--- src/Event/ContaoSubscriber.php
+++ src/Event/ContaoSubscriber.php
@@ -1,13 +1,16 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: aschempp
- * Date: 22.07.14
- * Time: 23:21
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package Contao\ContaoBundle
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
 
 namespace Contao\ContaoBundle\Event;
-
 
 use Contao\ContaoBundle\Exception\IncompleteInstallationException;
 use Contao\ContaoBundle\Exception\InsecureDocumentRootException;
